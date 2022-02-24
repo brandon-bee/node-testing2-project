d09278f059b4941221310788f5bab007a528f68e
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-24 13:47:04 -0800

    wrote model and tests for model

--- api/jokes/jokes.test.js
+++ api/jokes/jokes.test.js
@@ -1,7 +1,87 @@
 const request = require('supertest')
 const db = require('../../data/db-config')
 const server = require('../server')
+const Jokes = require('./jokes-model')
 
-test('is the correct environment for the tests', () => {
+const joke1 = {
+  joke: 'Why did the chicken run across the road?',
+  punchline: 'To get to the other side faster.'
+}
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db('jokes').truncate()
+  await db.seed.run()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
+it('is the correct environment for the tests', () => {
   expect(process.env.NODE_ENV).toBe('testing')
+})
+
+describe('jokes model functions', () => {
+  describe('Jokes.getAll', () => {
+    it('resolves to all jokes in the jokes table', async () => {
+      const jokes = await Jokes.getAll()
+      expect(jokes).toHaveLength(3)
+    })
+    it('resolves to the expected shape', async () => {
+      const jokes = await Jokes.getAll()
+      expect(jokes[0]).toHaveProperty('joke_id')
+      expect(jokes[0]).toHaveProperty('joke')
+      expect(jokes[0]).toHaveProperty('punchline')
+    })
+  })
+  describe('Jokes.getById', () => {
+    it('resolves to one joke', async () => {
+      const joke = await Jokes.getById(1)
+      expect(joke).toMatchObject({
+        joke_id: 1,
+        joke: 'What did the fish say when he swam into a wall?',
+        punchline: 'Dam.'
+      })
+    })
+  })
+  describe('Jokes.create', () => {
+    it('adds a new joke to the jokes table', async () => {
+      await Jokes.create(joke1)
+      const jokes = await Jokes.getAll()
+      expect(jokes).toHaveLength(4)
+    })
+    it('resolves to the newly created joke', async () => {
+      const newJoke = await Jokes.create(joke1)
+      expect(newJoke).toMatchObject(joke1)
+    })
+  })
+})
+
+describe('jokes router functions', () => {
+  describe('[GET] /api/jokes', () => {
+    it('', async () => {
+      expect(true).toBe(false)
+    })
+    it('', async () => {
+      expect(true).toBe(false)
+    })
+  })
+  describe('[GET] /api/jokes/:joke_id', () => {
+    it('', async () => {
+      expect(true).toBe(false)
+    })
+  })
+  describe('[POST] /api/jokes', () => {
+    it('', async () => {
+      expect(true).toBe(false)
+    })
+    it('', async () => {
+      expect(true).toBe(false)
+    })
+  })
 })
--- api/server.js
+++ api/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const server = express();
-
+const jokesRouter = require('./jokes/jokes-router')
 
 server.use(express.json());
-
+server.use('/api/jokes', jokesRouter)
 
 module.exports = server
